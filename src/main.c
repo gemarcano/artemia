ab767f560435f770567556952a9cb004dc946c5b
Gabriel Marcano <gabemarcano@yahoo.com>
2023-08-05 22:24:11 -0700

    artemia: adc gpios and adc support, save and load
     - Implement ADC support for V_RTC and V_ADP, finally
     - Implement support for saving and loading timestamps from filesystem
     - Increase flash clock to 24MHz
     - Fix warnings

--- src/main.c
+++ src/main.c
@@ -30,6 +30,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <time.h>
+#include <inttypes.h>
 
 static struct spi_bus spi_bus;
 static struct spi_device flash_spi;
@@ -43,8 +44,14 @@ static struct asimple_littlefs fs;
 static struct flash flash;
 static struct bmp280 bmp280;
 static struct adc adc;
+static struct gpio adc_enable_vadp;
+static struct gpio adc_enable_vrtc;
 static struct pdm pdm;
 static struct fft fft;
+
+static const uint8_t PHOTORES_PIN = 16;
+static const uint8_t VADP_PIN = 29;
+static const uint8_t VRTC_PIN = 11;
 
 // Convert tv_sec, which is a long representing seconds, to a string in buffer
 // Make sure to initialize buffer before calling
@@ -95,7 +102,7 @@ static int task_get_temperature_data(void* data)
 	uint32_t raw_temp = bmp280_get_adc_temp(&bmp280);
 	uint32_t compensate_temp = (uint32_t) (bmp280_compensate_T_double(&bmp280, raw_temp) * 1000);
 	write_csv_line(tfile, compensate_temp);
-	printf("TEMP: %u\r\n", compensate_temp);
+	printf("TEMP: %"PRIu32"\r\n", compensate_temp);
 
     fclose(tfile);
 	return 0;
@@ -121,7 +128,7 @@ static int task_get_pressure_data(void* data)
 	uint32_t raw_press = bmp280_get_adc_pressure(&bmp280);
 	uint32_t compensate_press = (uint32_t) (bmp280_compensate_P_double(&bmp280, raw_press, raw_temp));
 	write_csv_line(pfile, compensate_press);
-	printf("PRESS: %u\r\n", compensate_press);
+	printf("PRESS: %"PRIu32"\r\n", compensate_press);
 
 	fclose(pfile);
 	return 0;
@@ -144,20 +151,20 @@ static int task_get_light_data(void* data)
 
 	// Read current resistance of the Photo Resistor and write to flash
 	uint32_t adc_data[1] = {0};
-	uint8_t pins[] = {16};
+	uint8_t pins[] = {PHOTORES_PIN};
 	uint32_t resistance;
 
 	adc_trigger(&adc);
 	while (!(adc_get_sample(&adc, adc_data, pins, 1))){
 		am_util_stdio_printf("you shouldn't be here\r\n");
 	}
-	const double reference = 1.5;
+	const double reference = 2.0;
 	double voltage = adc_data[0] * reference / ((1 << 14) - 1);
 	am_util_stdio_printf("VOLTAGE: %f\r\n", voltage);
 	resistance = (uint32_t)((10000 * voltage)/(3.3 - voltage));
 
 	write_csv_line(lfile, resistance);
-	printf("RESISTANCE: %u\r\n", resistance);
+	printf("RESISTANCE: %"PRIu32"\r\n", resistance);
 
 	fclose(lfile);
 	return 0;
@@ -208,7 +215,7 @@ static int task_get_microphone_data(void* data)
     }
 	// Save frequency with highest amplitude to flash
 	write_csv_line(mfile, max);
-	printf("FREQ: %u\r\n", max);
+	printf("FREQ: %"PRIu32"\r\n", max);
 
 	fclose(mfile);
 	return 0;
@@ -304,19 +311,57 @@ static struct scron_tasks tasks = {
 	.size = ARRAY_SIZE(tasks_),
 };
 
-// FIXME implement loading from file
+/**
+ * The basic gist is to read back the last 2 time_t saved to the file, and if
+ * they match we likely don't have any corruption. If they don't go back until
+ * we find a matching valid pair.
+ */
 static void load_callback(const char *name, time_t *last_run)
 {
-	(void)name;
-	*last_run = 1;
+	char *buf = malloc(strlen(name) + 4 + 1);
+	memcpy(buf, "fs:/", 4);
+	memcpy(buf + 4, name, strlen(name) + 1);
+	FILE *file = fopen(buf, "r");
+	free(buf);
+	if (!file)
+	{
+		*last_run = 0;
+		return;
+	}
+	fseek(file, -16, SEEK_END);
+	time_t time1, time2;
+	static_assert(sizeof(time1) == 8, "wrong time_t size guess");
+	do
+	{
+		fread(&time1, sizeof(time1), 1, file);
+		fread(&time2, sizeof(time1), 1, file);
+		fseek(file, -32, SEEK_CUR);
+		// FIXME implement tellg so we can tell when we've gone too far back
+	} while (time1 != time2);
+	fclose(file);
+
+	*last_run = time1;
 }
 
-
-// FIXME implement saving to file
+/**
+ * The basic gist is to append to the file (FIXME what if we run out of space?)
+ * two time_t, which are checked by load_callback.
+ */
 static void save_callback(const char *name, time_t last_run)
 {
-	(void)name;
-	(void)last_run;
+	char *buf = malloc(strlen(name) + 4 + 1);
+	memcpy(buf, "fs:/", 4);
+	memcpy(buf + 4, name, strlen(name) + 1);
+	FILE *file = fopen(buf, "a");
+	free(buf);
+	if (!file)
+	{
+		// FIXME we should somehow alert that there is a bug or issue
+		return;
+	}
+	fwrite(&last_run, sizeof(last_run), 1, file);
+	fwrite(&last_run, sizeof(last_run), 1, file);
+	fclose(file);
 }
 
 __attribute__((constructor))
@@ -329,10 +374,11 @@ static void redboard_init(void)
 	am_bsp_low_power_init();
 	am_hal_sysctrl_fpu_enable();
 	am_hal_sysctrl_fpu_stacking_enable(true);
+	power_control_init(&power_control, 42, 43);
 
 	spi_bus_init(&spi_bus, 0);
 	spi_bus_enable(&spi_bus);
-	spi_bus_init_device(&spi_bus, &flash_spi, SPI_CS_2, 4000000u);
+	spi_bus_init_device(&spi_bus, &flash_spi, SPI_CS_2, 24000000u);
 	spi_bus_init_device(&spi_bus, &bmp280_spi, SPI_CS_1, 4000000u);
 	spi_bus_init_device(&spi_bus, &rtc_spi, SPI_CS_3, 2000000u);
 	am1815_init(&rtc, &rtc_spi);
@@ -345,37 +391,39 @@ static void redboard_init(void)
 		asimple_littlefs_format(&fs);
 		asimple_littlefs_mount(&fs);
 	}
-	syscalls_rtc_init(&rtc);
-	syscalls_uart_init(&uart);
-	syscalls_littlefs_init(&fs);
-	power_control_init(&power_control, 42, 43);
-	// FIXME remove debug
 	uart_init(&uart, UART_INST0);
-	// FIXME remove debug
-	am_util_stdio_printf("HELLO\r\n");
-
-	scron_init(&scron, &tasks);
-	scron_load(&scron, load_callback);
-
-	// Initialize the structs that weren't originally in the file
 	bmp280_init(&bmp280, &bmp280_spi);
-	uint8_t pins[] = {16};
-	adc_init(&adc, pins, 1);
+	uint8_t pins[] = {PHOTORES_PIN, VADP_PIN, VRTC_PIN};
+	adc_init(&adc, pins, ARRAY_SIZE(pins));
 	pdm_init(&pdm);
 	fft_init(&fft);
 
-	// Trigger the ADC to start collecting data
-	adc_trigger(&adc);
+	gpio_init(&adc_enable_vrtc, 0, GPIO_MODE_OUTPUT, 1);
+	gpio_init(&adc_enable_vadp, 1, GPIO_MODE_OUTPUT, 1);
+
+	syscalls_rtc_init(&rtc);
+	syscalls_uart_init(&uart);
+	syscalls_littlefs_init(&fs);
 
 	// After init is done, enable interrupts
 	am_hal_interrupt_master_enable();
+
+	scron_init(&scron, &tasks);
+	scron_load(&scron, load_callback);
 }
 
 __attribute__((destructor))
 static void redboard_shutdown(void)
 {
+	gpio_set(&adc_enable_vrtc, false);
+	gpio_set(&adc_enable_vadp, false);
 	scron_save(&scron, save_callback);
 	power_control_shutdown(&power_control);
+}
+
+static double convert_adc_voltage(uint32_t sample)
+{
+	return (sample * 2.0) / 16383u;
 }
 
 int main(void)
@@ -383,22 +431,23 @@ int main(void)
 	for(;;)
 	{
 		// Get timestamp from RTC and last task run
-		// Get current battery voltage FIXME
-		double current_voltage = 2.0;
+		uint32_t adc_data[2] = {0};
+		uint8_t pins[] = {VRTC_PIN, VADP_PIN};
+		adc_trigger(&adc);
+		while (!(adc_get_sample(&adc, adc_data, pins, ARRAY_SIZE(pins))));
+		double current_voltage = convert_adc_voltage(adc_data[1]);
 		struct timeval now = am1815_read_time(&rtc);
 		time_t now_s = now.tv_sec;
-
-		uint8_t buffer[21] = {0};
-		time_to_string(buffer, (uint64_t)now_s);
-		//printf("%s\r\n", buffer);
 
 		bool ran_task = artemia_scheduler(&scron, current_voltage, now_s);
 		if (!ran_task)
 		{
 			// Reconfigure the alarm
 			time_t next = scron_next_time(&scron);
-			// FIXME update am1815 RTC alarm
-			//break;
+			struct timeval tv = { .tv_sec = next, };
+			am1815_write_alarm(&rtc, &tv);
+			// FIXME we need to adjust the alarm's repeat function
+			break;
 		}
 	}
 }
